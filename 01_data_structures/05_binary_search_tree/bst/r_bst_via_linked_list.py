4760f725419f31595c3af56f28b91e210cf80fe1
Dheemanth Bhat <d.g.bhat094@gmail.com>
2025-06-10 10:47:41 +0530

    Recursive function for BST delete.

--- 01_data_structures/05_binary_search_tree/bst/r_bst_via_linked_list.py
+++ 01_data_structures/05_binary_search_tree/bst/r_bst_via_linked_list.py
@@ -11,11 +11,20 @@ class BinarySearchTree:
     Definition for Binary Search Tree data structure with recursive implementation.
     """
 
+    # Delete strategies.
+    IN_ORDER_SUCCESSOR: int = 1
+    IN_ORDER_PREDECESSOR: int = 2
+
     # Create
-    def __init__(self, allow_duplicates: bool = False):
+    def __init__(
+        self,
+        allow_duplicates: bool = False,
+        delete_strategy: int = IN_ORDER_SUCCESSOR,
+    ):
         self.root: Node = None
         self.level: int = 0
         self.allow_duplicates: bool = allow_duplicates
+        self.delete_strategy = delete_strategy
 
     def _create_node(self, value: Any) -> Node:
         """
@@ -47,6 +56,38 @@ class BinarySearchTree:
         contains an item matching the `value`.
         """
         return self._contains(self.root, value)
+
+    def get_min(self, cur_node: Node) -> Node:
+        """
+        Function return node with smallest value.
+        """
+        while cur_node.left is not None:
+            cur_node = cur_node.left
+
+        return cur_node
+
+    def get_max(self, cur_node: Node) -> Node:
+        """
+        Function return node with largest value.
+        """
+        while cur_node.right is not None:
+            cur_node = cur_node.right
+
+        return cur_node
+
+    def _get_successor(self, curr_node: Node) -> Node:
+        """
+        Function to find and return successor node from its
+        parent node based on pre-selected delete strategy.
+        """
+        if self.delete_strategy == self.IN_ORDER_SUCCESSOR:
+            # Get in-order-successor.
+            return self.get_min(curr_node.right)
+        elif self.delete_strategy == self.IN_ORDER_PREDECESSOR:
+            # Get in-order-predecessor.
+            return self.get_max(curr_node.left)
+        else:
+            raise ValueError(f"Invalid delete strategy: {self.delete_strategy}")
 
     # Update
     def _insert(
@@ -112,3 +153,49 @@ class BinarySearchTree:
         STEP 4: Update level.
         """
         self.level = max(curr_level, self.level)
+
+    # Delete
+    def __delete(self, tgt_node: Node):
+        """
+        Recursive function to delete the node in BST matching the `value`.
+        """
+        # Case #1: Leaf node.
+        if tgt_node.left is None and tgt_node.right is None:
+            return None
+
+        # Case #2: Half node.
+        elif tgt_node.left is None:
+            return tgt_node.right
+        elif tgt_node.right is None:
+            return tgt_node.left
+
+        # Case #3: Full node.
+        elif tgt_node.left is not None and tgt_node.right is not None:
+            ssr_node = self._get_successor(tgt_node)
+            ssr_node.left, ssr_node.right = tgt_node.left, tgt_node.right
+
+    def __locate(self, curr_node: Node, value: Any):
+        """
+        Recursive function to locate the node in BST matching the `value`.
+        """
+        if curr_node is None:
+            return None
+        elif value == curr_node.value:
+            curr_node = self.__delete(curr_node)
+        elif value < curr_node.value:
+            curr_node.left = self.__locate(curr_node.left, value)
+        elif value > curr_node.value:
+            curr_node.right = self.__locate(curr_node.right, value)
+
+        return curr_node
+
+    def delete(self, value: Any) -> bool:
+        """
+        Function to delete the node in BST matching the `value`.
+        """
+        tgt_node = self.__locate(self.root, value)
+
+        if tgt_node is None:
+            return False
+
+        return True
